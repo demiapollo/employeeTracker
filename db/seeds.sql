b82ac58e79317b2a698a8fa3b0d582afacae50a1
demiapollo <apollodemirel67@gmail.com>
2023-05-09 11:29:25 -0700

    connection added

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+    ('Marketing');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Salesperson', 80000.00, 1),
+    ('Lead Engineer', 150000.00, 2),
+    ('Software Engineer', 120000.00, 2),
+    ('Finance Specialist', 125000.00, 3),
+    ('Legal Team Lead', 250000.00, 4),
+    ('Lawyer', 190000.00, 4),
+    ('Marketing Lead', 190000.00, 5),
+    ('Marketing Specialist', 150000.00, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, 1),
+    ('Kevin', 'Tupik', 4, 1),
+    ('Malia', 'Brown', 5, 2),
+    ('Sarah', 'Lourd', 6, 2),
+    ('Tom', 'Allen', 7, 2),
+    ('Sam', 'Carter', 8, 2);
