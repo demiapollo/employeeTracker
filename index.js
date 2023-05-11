32b8d9fbfb5280750d07ec57cff7ad98458b2c24
demiapollo <apollodemirel67@gmail.com>
2023-05-11 10:30:29 -0700

    functions updated

--- index.js
+++ index.js
@@ -1,44 +1,9 @@
 const db = require("./config/connection");
 const inquirer = require("inquirer");
-const prompts = require("./prompts");
-console.log(prompts[0]);
-
-
-//select task
-// function selectTask() {
-//   inquirer.prompt(prompts[0]).then((answers) => {
-//     switch (answers.task) {
-//       case "View all departments":
-//         viewDepartments();
-//         break;
-//       case "View all roles":
-//         viewRoles();
-//         break;
-//       case "View all employees":
-//         viewEmployees();
-//         break;
-//       case "Add a department":
-//         addDepartment();
-//         break;
-//       case "Add a role":
-//         addRole();
-//         break;
-//       case "Add an employee":
-//         addEmployee();
-//         break;
-//       case "Update an employee role":
-//         updateEmployeeRole();
-//         break;
-//       case "Exit":
-//         db.end();
-//         break;
-//     }
-//   });
-// }
+const selectTask = require("./lib/selectTask");
 
 //start application
 function init() {
-
   selectTask();
 }
 
