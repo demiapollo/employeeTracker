9d6faea6984cc93b3ce9743a7dcd97259d34f758
demiapollo <apollodemirel67@gmail.com>
2023-05-11 12:00:06 -0700

    salary decimal issue

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,14 +8,14 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Salesperson', 80000.00, 1),
-    ('Lead Engineer', 150000.00, 2),
-    ('Software Engineer', 120000.00, 2),
-    ('Finance Specialist', 125000.00, 3),
-    ('Legal Team Lead', 250000.00, 4),
-    ('Lawyer', 190000.00, 4),
-    ('Marketing Lead', 190000.00, 5),
-    ('Marketing Specialist', 150000.00, 5);
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Finance Specialist', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4),
+    ('Marketing Lead', 190000, 5),
+    ('Marketing Specialist', 150000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
